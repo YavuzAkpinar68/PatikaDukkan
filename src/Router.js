64a7ff21df49c032df64177997f51e1371cbfc19
yavuz <yavuz.akpinar.68@gmail.com>
2021-11-18 15:09:42 +0300

    App.js turned to Router.js for navigation

---
+++ src/Router.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Products from "./pages/products/products";
+import Detail from "./pages/detail";
+
+const Stack = createNativeStackNavigator();
+
+const Router = () => {
+  return(
+   <NavigationContainer>
+     <Stack.Navigator>
+       <Stack.Screen name="ProductsPage" component={Products} />
+       <Stack.Screen name="DetailPage" component={Detail}/>
+     </Stack.Navigator>
+   </NavigationContainer>
+  )
+}
+export default Router
